9608f34466d1d449d77293a54be0edd21fd06e9c
Florence Delattre <flo-qumal@hotmail.fr>
2019-11-15 10:11:41 +0100

    first commit

---
+++ zoo/Giraffe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoo
+{
+    class Giraffe : Prey
+    {
+        public Giraffe(string sex) : base("Giraffe", sex)
+        { }
+
+
+        public override void MakeBaby(Prey giraffe1, Prey giraffe2)
+        {
+            // Boucler sur toutes les girafes puis if pour que les sexes soient différents
+            // return new Giraffe;
+            throw new NotImplementedException();
+        }
+    }
+}
